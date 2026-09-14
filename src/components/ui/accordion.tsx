70e1aded6eabfdb00572bc85cb7f0bd9802e4351
Paul Suceveanu <p.suceveanu@gmail.com>
2026-09-14 13:50:07 +0300

    warn fix

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -28,7 +28,7 @@ const AccordionTrigger = React.forwardRef<
       {...props}
     >
       {children}
-      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-300 ease-[cubic-bezier(0.16,1,0.3,1)]" />
+      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-300" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ));
